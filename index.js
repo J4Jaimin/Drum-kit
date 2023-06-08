2cbfaa580279955837baa0acfee9e980c4897607
Jaimin-1102 <jaimin.rana@agshealth.com-at-205008539362>
2023-06-08 13:37:49 +0530

    first commit with html, css and js files..

---
+++ index.js
@@ -0,0 +1,44 @@
+
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+
+        var buttoninnerHTML = this.innerHTML;
+
+        switch (buttoninnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+        }
+    });
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
